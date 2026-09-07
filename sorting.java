be55d92e74800d4324620577144027c4b06d8eee
Aryanaj13 <octaryjain@gmail.com>
2026-09-07 20:05:27 +0530

    sorting.java

--- sorting.java
+++ sorting.java
@@ -30,6 +30,18 @@ public class sorting {
             arr[minindex] = temp;
         }
     }
+    //insertion sort
+    public static void insertionsort(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while (prev>=0 && arr[prev]>curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+    }
     //print an array for sorrting
     public static void printArray(int[] arr){
         for(int i=0;i<=arr.length-1;i++){
