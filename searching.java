c3778bfa9d96de299f6f6965cf90deac684ec555
Aryan Jain <octaryjain@gmail.com>
2026-09-11 19:47:11 +0530

    Add searching problems

---
+++ searching.java
@@ -0,0 +1,57 @@
+public class searching {
+    //linear search
+    public static int linearsearch(int[] arr, int key){
+        int i;
+        for(i=0;i<=arr.length-1;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    //binary search
+    public static int binarysearch(int[] arr, int key){
+        int st = 0;
+        int end = arr.length-1;
+    
+        while (st<=end) {
+            int mid = (st+end)/2;
+            if(arr[mid] == key){
+                return mid;
+            }else if(arr[mid] < key){
+                st = mid+1;
+
+            }else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+     //rotated and sorted array
+    public static int rotatedsortedarr(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(arr[mid] == target){
+                return mid;
+            } 
+            else if(arr[start] <= arr[mid]){
+                if(arr[start] <= target && target <arr[mid]){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(arr[mid] <= target && target <= arr[end]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    
+}
