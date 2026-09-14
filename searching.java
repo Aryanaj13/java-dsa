820604f3ce5d14f69db1981e62e6f2860beef83c
Aryanaj13 <octaryjain@gmail.com>
2026-09-14 20:48:51 +0530

    Update searching.java

--- searching.java
+++ searching.java
@@ -51,6 +51,25 @@ public class searching {
                 }
             }
         }
+         return -1;
+    }
+    
+        //searxh inserrt position
+public static int searinspos(int[] arr, int target){
+    int st =0;
+    int end = arr.length-1;
+    while(st<=end){
+        int mid = st+(end-st)/2;
+        if(arr[mid] == target){
+            return mid;
+        }else if(arr[mid] < target){
+            st = mid+1;
+        }else{
+            end = mid-1;
+        }
+    }
+    return st;
+}
         return -1;
     }
     
